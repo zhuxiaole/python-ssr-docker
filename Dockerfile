33e04c21bbca716dc0bfea997e15ce79e0ef67ff
zhuxiaole <zhuxiaole@zhuxiaole.org>
2018-03-07 01:02:01 +0800

    add udp2raw udpspeeder

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,15 @@ ENV TIMEOUT         300
 ENV DNS_ADDR        8.8.8.8
 ENV DNS_ADDR_2      8.8.4.4
 
+#GAME: 游戏（3倍流量）  2: NORMAL（视频、下载、网页，1.5倍流量） 3: COMPROMISE（折中，1.6倍流量） 4: GAME_NO_LIMIT（大于3倍流量）
+ENV UDPSPEEDER_MODE GAME
+ENV UDPSPEEDER_VERSION v2@20171125.0
+ENV UDPSPEEDER_PORT 4096
+ENV UDPSPEEDER_PASSWORD psw
+
+ENV UDP2RAW_VERSION 20180225.0
+ENV UDP2RAW_PORT 7123
+
 RUN apt-get update
 RUN apt-get install -y python
 RUN apt-get install -y wget
@@ -24,6 +33,10 @@ RUN wget -qO- --no-check-certificate https://download.libsodium.org/libsodium/re
 WORKDIR /libsodium-stable
 RUN ./configure && make -j2 && make install
 RUN ldconfig
+
+RUN mkdir /udpspeeder
+RUN wget -qO- --no-check-certificate https://github.com/wangyu-/UDPspeeder/releases/download/$UDPSPEEDER_VERSION/speederv2_binaries.tar.gz | tar -xzf - -C /udpspeeder
+
 
 ADD start.sh /start.sh
 RUN chmod a+x /*.sh
--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+if [ "${UDPSPEEDER_MODE}" == "GAME" ]; then
+	./speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f2:4 --timeout 1
+fi
+if [ "${UDPSPEEDER_MODE}" == "NORMAL" ]; then
+	./speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f20:10 --timeout 8
+fi
+if [ "${UDPSPEEDER_MODE}" == "COMPROMISE" ]; then
+	./speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f10:6 --timeout 3
+fi
+if [ "${UDPSPEEDER_MODE}" == "GAME_NO_LIMIT" ]; then
+	./speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f2:4 --timeout 0
+fi
+
+./udp2raw_amd64 -s -l0.0.0.0:$UDP2RAW_PORT  -r127.0.0.1:$UDPSPEEDER_PORT   -a -k "$UDP2RAW_PASSWORD" --raw-mode faketcp --cipher-mode xor
 
 python /shadowsocksr-manyuser/shadowsocks/server.py -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM
