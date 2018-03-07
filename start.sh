79210ec99f8f8734d0909bc4f1465d384555f1b1
zhuxiaojun <zhu.xj@neusoft.com>
2018-03-07 10:50:41 +0800

    测试：udp2raw和udpspeeder后台运行

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 if [ "${UDPSPEEDER_MODE}" == "GAME" ]; then
-	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f2:4 --timeout 1
+	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f2:4 --timeout 1 &
 fi
 if [ "${UDPSPEEDER_MODE}" == "NORMAL" ]; then
-	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f20:10 --timeout 8
+	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f20:10 --timeout 8 &
 fi
 if [ "${UDPSPEEDER_MODE}" == "COMPROMISE" ]; then
-	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f10:6 --timeout 3
+	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f10:6 --timeout 3 &
 fi
 if [ "${UDPSPEEDER_MODE}" == "GAME_NO_LIMIT" ]; then
-	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f2:4 --timeout 0
+	/udpspeeder/speederv2 -s -l0.0.0.0:$UDPSPEEDER_PORT -r127.0.0.1:$SERVER_PORT   -k "$UDPSPEEDER_PASSWORD"  -f2:4 --timeout 0 &
 fi
 
-/udp2raw/udp2raw_amd64 -s -l0.0.0.0:$UDP2RAW_PORT  -r127.0.0.1:$UDPSPEEDER_PORT   -a -k "$UDP2RAW_PASSWORD" --raw-mode faketcp --cipher-mode xor
+/udp2raw/udp2raw_amd64 -s -l0.0.0.0:$UDP2RAW_PORT  -r127.0.0.1:$UDPSPEEDER_PORT   -a -k "$UDP2RAW_PASSWORD" --raw-mode faketcp --cipher-mode xor &
 
 python /shadowsocksr-manyuser/shadowsocks/server.py -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM
