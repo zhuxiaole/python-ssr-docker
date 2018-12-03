b9cfb40fcdfd0b5a1ee76ae7c2847d4a4fddd22b
朱晓军 <1365441936@qq.com>
2018-12-03 09:07:54 +0800

    Revert "使用ssrr版本；后台运行；"
    This reverts commit 4192d5eb0b0ca1bd7bdc419bcecbc5fd56f09aca.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup python /shadowsocksr-manyuser/shadowsocks/server.py -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM >> ssserver.log 2>&1 &
+python /shadowsocksr-manyuser/shadowsocks/server.py -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM
